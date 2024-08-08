6b8407b4a85faba8478db7eabdf476d267398bca
Zayne <star1582@naver.com>
2024-08-08 09:04:42 +0100

    Created the outline #5

--- src/Email.java
+++ src/Email.java
@@ -1,2 +1,20 @@
 public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    // Constructor to receive the first name and last name
+
+    // Ask for the department
+
+    // Generate a random password
+
+    // Set the mailbox capacity
+
+    // Set the alternate email
+
+    // Change the password
 }
