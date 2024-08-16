346780ebfbb5211024240e45655d2a213775360f
Zayne <star1582@naver.com>
2024-08-16 11:11:27 +0100

    Implementing class set methods #14

--- src/Email.java
+++ src/Email.java
@@ -6,7 +6,7 @@ public class Email {
     private String password;
     private String department;
     private String email;
-    private int mailboxCapacity;
+    private int mailboxCapacity = 500;
     private String alternateEmail;
     private int defaultPasswordLength = 10;
     private String companySuffix = "aeycompany.com";
@@ -56,10 +56,18 @@ public class Email {
         return new String(password);
     }
 
-
     // Set the mailbox capacity
+    public void setMailboxCapacity(int capacity) {
+        this.mailboxCapacity = capacity;
+    }
 
     // Set the alternate email
+    public void setAlternateEmail(String altEmail) {
+        this.alternateEmail = altEmail;
+    }
 
     // Change the password
+    public void changePassword(String password) {
+        this.password = password;
+    }
 }
